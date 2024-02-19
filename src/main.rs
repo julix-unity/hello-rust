588c9e423190ce19fbe0bf4b4c8fc8331ab4fb55
Julian Sloman <julix1991@gmail.com>
2024-02-18 23:34:00 -0800

    play with hash map and median fn

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@ use guessing_game::guessing_game;
 
 pub mod guessing_game;
 
-use median_and_mode::{get_median};
+use median_and_mode::{get_median, get_mode};
 
 pub mod median_and_mode;
 
 
-const run_guessing_game: bool = false;
+const RUN_GUESSING_GAME: bool = false;
 
 fn main() {
-    if run_guessing_game {
+    if RUN_GUESSING_GAME {
         guessing_game();
     }
 
-    let median = get_median(vec![1,2,3,4,2, 5, 5, 5,]);
-    println!("{median}");
+    let vector = vec![1,2,3,4,2, 5, 5, 5,];
+    let median = get_median(vector.clone());
+    let mode = get_mode(vector);
+    println!("Median: {median} and Mode: {:?}", {mode});
 }
--- src/median_and_mode.rs
+++ src/median_and_mode.rs
@@ -10,14 +10,29 @@ pub fn get_median (mut list: Vec<i32>) -> f32 {
     // median
     list.sort();
     let length = list.len();
-    let isEven = length % 2 == 0;
+    let is_even = length % 2 == 0;
     
-    match isEven {
+    match is_even {
         true => (list[length / 2] as f32 + list[(length / 2) - 1] as f32) / 2.0,
         false => list[length / 2] as f32
     }
 }
 
-fn get_mode (mut list: Vec<i32>) {
-    
+pub fn get_mode (list: Vec<i32>) -> Vec<i32> {
+    let mut num_map = HashMap::new();
+
+    // populate map
+    for &number in &list {
+        let count = num_map.entry(number).or_insert(0);
+        *count += 1
+    }
+
+    // Find the highest count
+    let max_count = num_map.values().cloned().max().unwrap_or(0);
+
+    // Collect all numbers with the highest count
+    num_map.into_iter()
+           .filter(|&(_, count)| count == max_count)
+           .map(|(num, _)| num)
+           .collect()
 }
