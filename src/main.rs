f23569e15efeefb9622e4a9eaef1242e8811b770
Julian Sloman <julix1991@gmail.com>
2024-02-18 19:30:01 -0800

    improve median fn

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,22 @@ fn main() {
         guessing_game();
     }
 
-    get_median(vec![1,2,3,4,2, 5, 5, 5,])
+    let median = get_median(vec![1,2,3,4,2, 5, 5, 5,]);
+    println!("{median}");
 }
 
     /*
      * Given a list of integers, use a vector and return the median (when sorted, the value in the middle position) and
      */
-fn get_median (mut list: Vec<i32>) {
+fn get_median (mut list: Vec<i32>) -> f32 {
 
     // median
     list.sort();
     let length = list.len();
-    let isEven = &length % 2 == 0;
-    let median = match isEven {
-        true => (list[length / 2] + list[(length / 2) - 1]) / 2,
-        false => list[length / 2] 
+    let isEven = length % 2 == 0;
+    
+    match isEven {
+        true => (list[length / 2] as f32 + list[(length / 2) - 1] as f32) / 2.0,
+        false => list[length / 2] as f32
     };
-    println!("{median}");
 }
