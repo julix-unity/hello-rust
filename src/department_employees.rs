e6e5535c77e80f14193aeb8c2b3f8018c47e2d7a
Julian Sloman <julix1991@gmail.com>
2024-02-27 03:47:43 -0800

    Refactor department employee challenge...
    * better use of Strings vs &str
    * lifted function up
    * better use of entry vs [] for reading or inserting manually
    * remove unused template code
    * add handling for displaying a department that doesn't exist

--- src/department_employees.rs
+++ src/department_employees.rs
@@ -1,3 +1,37 @@
+use std::collections::HashMap;
+use std::io;
+use dialoguer::Select;
+
+fn prompt_and_read(input_prompt: &str) -> String {
+    println!("{}", input_prompt);
+    let mut input_string = String::new();
+    io::stdin()
+        .read_line(&mut input_string)
+        .expect("Failed to read line");
+    input_string.trim().to_string()
+}
+
+fn add_employee_to_department(employees_by_department_map: &mut HashMap<String, Vec<String>>) {
+    let employee_name = prompt_and_read("What's the name of the employee you want to add?");
+    let department_prompt = format!("Great! And what's the name of the department you want to add {} to?", employee_name);
+    let department_name = prompt_and_read(&department_prompt);
+
+    employees_by_department_map.entry(department_name.clone())
+        .and_modify(|e| {
+            match e.binary_search(&employee_name) {
+                Ok(_) => println!("{} is already in the {} department.", employee_name, department_name),
+                Err(pos) => e.insert(pos, employee_name.clone()),
+            }
+        })
+        .or_insert_with(|| vec![employee_name.clone()]);
+    
+    println!("{} has been added to the {} department.", employee_name, department_name);
+
+    if let Some(employees) = employees_by_department_map.get(&department_name) {
+        println!("{} is now in the list: {:?}", &employee_name, employees);
+    }
+}
+
 /*
 * Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company. 
 * For example, “Add Sally to Engineering” or “Add Amir to Sales.” 
@@ -5,32 +39,24 @@
 * Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.
 */
 
-use std::collections::HashMap;
-use std::{cmp::Ordering, io};
-use dialoguer::Select;
-
-// TODO look into this:
-// template< typename T >
-// typename std::vector<T>::iterator 
-//    insert_sorted( std::vector<T> & vec, T const& item )
-// {
-//     return vec.insert
-//         ( 
-//             std::upper_bound( vec.begin(), vec.end(), item ),
-//             item 
-//         );
-// }
 
 pub fn department_employees() {
 
     // Setup
     let mut employees_by_department_map = HashMap::from([
-        ("Engineering", vec!("Emily", "Frank", "Sarah")),
-        ("Marketing", vec!("Boo", "Marleen")),
+        (String::from("Engineering"), vec![
+            String::from("Emily"), 
+            String::from("Frank"),
+            String::from("Sarah"),
+        ]),
+        (String::from("Marketing"), vec![
+            String::from("Boo"),
+            String::from("Marleen"),
+        ]),
     ]);
 
 
-    let options: Vec<&str> = vec!["1) Display employees of a particualar department", "2) Display all employees", "3) Add an employee to a department", /*"4) Remove an employee"*/];
+    let options: Vec<&str> = vec!["1) Display employees of a particular department", "2) Display all employees", "3) Add an employee to a department", /*"4) Remove an employee"*/];
 
     let selection = Select::new()
         .with_prompt("What do you choose?")
@@ -38,65 +64,30 @@ pub fn department_employees() {
         .interact()
         .unwrap() + 1;
 
-    let choose_department = || {
-        let departments = Vec::from_iter(employees_by_department_map.keys());
+    let choose_department = || -> String {
+        let departments: Vec<&String> = Vec::from_iter(employees_by_department_map.keys());
         let selection = Select::new()
-        .with_prompt("Diplay which department?")
+        .with_prompt("Display which department?")
         .items(&departments)
         .interact()
         .unwrap();
-        departments[selection]
+        departments[selection].clone()
     };
+
 
     // println!("{:?}", Vec::from_iter(employees_by_department_map.keys()));
     
     match selection {
         1 => {
             let department = choose_department();
-            let department_employees = &employees_by_department_map[department];
-            println!("The employees in {} are {:?}", department, department_employees);
+            if let Some(department_employees) = employees_by_department_map.get(&department) {
+                println!("The employees in {} are {:?}", department, department_employees);
+            } else {
+                println!("The department '{}' does not exist.", department);
+            }
         },
         2 => println!("{:?}", employees_by_department_map),
-        3 => {
-            println!("What's the name of the employee you want to add?");
-            
-            let mut employee_name_raw = String::new();
-            let mut department_name_raw = String::new();
-            io::stdin()
-                .read_line(&mut employee_name_raw)
-                .expect("Failed to read employee name");
-            
-            println!("Great! And what's the name of the department you want to add {} to?", employee_name_raw);
-            
-            io::stdin()
-                .read_line(&mut department_name_raw)
-                .expect("Failed to read department name");
-
-            let employee_name = employee_name_raw.trim();
-            let department_name = department_name_raw.trim();
-
-
-            let mut deparment_list = employees_by_department_map.keys();
-            let deparment_exists = deparment_list.find(|&&x| x == department_name);
-
-            match deparment_exists {
-                Some(_x) => {
-                    let mut employees = employees_by_department_map[department_name].clone();
-                    match employees.binary_search(&employee_name){
-                        Ok(_pos) => {}, // element already in vector @ `pos` 
-                        Err(pos) => {
-                            employees.insert(pos, employee_name);
-                            employees_by_department_map.insert(&department_name, employees);
-                        }
-                    }
-                }
-                None => {
-                    employees_by_department_map.insert(&department_name, vec!(&employee_name));
-                }
-            }
-
-            println!("Perfect, adding {} to {}!\n{} is now in the list: {:?}", employee_name, department_name, employee_name, employees_by_department_map[department_name]);
-        }
+        3 => add_employee_to_department(&mut employees_by_department_map),
         _ => {}
     }
 
